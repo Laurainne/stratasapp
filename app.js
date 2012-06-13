7e4342c0def4e98de9531a083e44e9f6ff939a30
Lauren Caswell <lauren.puppy@gmail.com>
2012-06-12 19:28:12 -0500

    Login

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-var strata = require("strata");
+var strata =require('strata');
+var inspect = require('uutil').inspect;
 
-strata.run(function (env, callback) {
-    var headers = {
-        "Content-Type": "text/plain",
-        "Content-Length": "12"
-    };
+var db = {
+	tim: "noderocks"
+	lauren:"michelle"
+}
+strata.use(strata.commonLogger);
 
-    callback(200, headers, "Hello world!");
+strata.use(strata.contentType, "text/plain")
+strata.use(strata.contentLength);
+strata.use(strata.sessionCookie, {
+    secret: "sup3rs3kret",
+    name: "myapp.session"
 });
+strata.use(strata.file, __dirname + "/Public");
+
+strata.post("/login", function (env, callback) {
+	var req = strata.Request(env);
+
+	req.params(function (err, params) {
+		if (db[params.username] === params.password) {
+			req.session.username= params.username;
+			strata.redirsct(env, callback, "/");
+		}
+		else {
+			strata.redirect(env, callback, "/login.htmml");
+;		}
+		console.log(params)
+	});
+
+});
+
+strata.run();
